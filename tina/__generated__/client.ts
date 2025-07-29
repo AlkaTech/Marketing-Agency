1e8351e35b64ae50695895d31dfac01d96070bf0
John Onaya <37826079+AlkaTech@users.noreply.github.com>
2025-07-30 00:07:08 +0300

    Delete tina directory

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'ba5d7400fec7bad3f63ccb8636ae5e64b02015d4', queries,  });
-export default client;
-  
